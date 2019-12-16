3310bbb4358db89c34353742b34a64cbb4dc7449
GeniferGonzalezSantos <genh_@hotmail.com>
2019-12-16 17:21:32 -0300

    adicionar arquivo componentes e printar menu na tela

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -1,46 +1,52 @@
 import React, { useState } from 'react';
 /* import logo from './logo.svg'; */
 import './App.css';
-import {db} from './firebase.js';
+import { db } from './firebase.js';
 
 function App() {
-  const [dados, setDados] = useState('');
-   db.collection('Menu')
-  .get()
-  .then((querySnapshot) => {
-    const banana = querySnapshot.forEach(doc => { 
-      (doc.data()) 
-    })
-    setDados(banana)
-  })
-    return (
-      <div>
-        <button onClick={() => setDados}> dados</button>
-        <div>{dados.map((lala) => 
-      <div key={lala.id}>{lala.price}</div>
-        )}
+  const [dados, setDados] = useState([]);
+  db.collection('Menu')
+    .get()
+    .then((snapshot) => {
+      const banana = snapshot.docs.map((doc) => ({
+        ...doc.data()
+      }))
+      /* (console.log(doc.data())) */
+      setDados(banana)
+    });
+
+  return (
+    <>
+      <div>{dados.map((item) =>
+        <div>
+          <br></br>
+          {item.name}
+          <br></br>
+          R${item.price}
         </div>
-        </div> 
+      )}
+      </div>
+    </>
 
 
-        /* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Learn React
-        </a>
-      </header>
-    </div> */
-  );
-    }
 
+    /* <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      Learn React
+    </a>
+  </header>
+</div> */
 
+  )
+};
 export default App;
---
+++ burguer-queen/src/components.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import App from './App.js'
+
+export default class tantoFaz extends React.Component {
+    render(){
+        
+    }
+}
