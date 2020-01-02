453fa672414ca4cadb4468da613d4b0d8e5c85f2
GeniferGonzalezSantos <genh_@hotmail.com>
2020-01-02 16:30:11 -0300

    adicionar arquivos componentes,rotas e excluir arquivos de react

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -1,65 +1,39 @@
-import React, { useState, useEffect } from 'react';
-/* import logo from './logo.svg'; */
-import './App.css';
-import { db } from './firebase.js';
-import { checkPropTypes } from 'prop-types';
-import Card from './componentes/Card/card';
-import { StyleSheet, css } from 'aphrodite'
-import Button from './componentes/Button/button';
+import React from 'react';
+import FazerPedido from './components/Routers/Cliente'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
-
-  const [dados, setDados] = useState([]);
-  const [item, setItem] = useState([])
-
-  useEffect(() => {
-    db.collection('Menu')
-      .get()
-      .then((snapshot) => {
-        const banana = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data()
-        }))
-        /* (console.log(doc.data())) */
-        setDados(banana)
-      });
-  }, [0])
-
-
-  const filterBreakfast = (event) => {
-    const seiLa = event.target.id;
-    const showBreakfast = (seiLa === 'breakfast')? true: false;
-    const itens = dados.filter((lala) => lala.breakfast === showBreakfast)
-    return setItem(itens)
-  }
-  console.log(item)
-
   return (
-    <>
-      <div key={'banana'}>
-        <Button
-          id='breakfast'
-          onClick={filterBreakfast}
-          children='breakfast'
-        />
-        <Button
-          id='btn'
-          onClick={filterBreakfast}
-          children='resto do dia'
-        />
-      </div>
-      <div key={dados.id}>{item.map((item) =>
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <br></br>
+                <Link to="/">Cliente</Link>
+              <br></br>
+                <Link to="/cozinha" log>Cozinha</Link>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/cozinha">
+              <div>peraí</div>
+            </Route>
+            <Route path="/">
+            <FazerPedido/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
 
-        <Card key={item.id}>{item.name}
-          <br></br>
-          R${item.price}</Card>
-
-      )}
-
-      </div>
-    </>
-
+    </div>
   )
 }
+
 export default App;
